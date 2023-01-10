5990b9b4ae2e16c2e0f49fb9f228b09f4459ea89
cuonghv2010 <cuonghv201095@gmail.com>
2023-01-10 22:16:54 +0900

    Delegate type basic
    Signed-off-by: cuonghv2010 <cuonghv201095@gmail.com>

---
+++ basic/delegate_basic/delegate_basic/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace delegate_basic
+{
+    internal class Program
+    {
+        delegate int MyDelegate(int a, int b);
+
+        static int TestMethodA(int x, int y)
+        {
+            System.Console.WriteLine("X + Y = "  + (x + y) + "\n");
+            return x + y;
+        }
+
+        static int TestMethodB(int x, int y)
+        {
+            System.Console.WriteLine("X * Y = " + (x * y) + "\n");
+            return x * y;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            MyDelegate md = null;
+
+            md += TestMethodA;
+            md += TestMethodB;
+
+            md.Invoke(1,2);
+
+        }
+    }
+}
