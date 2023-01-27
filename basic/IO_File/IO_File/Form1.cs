6a6e2f585f7ddf6a1b4aa11129020e3f7c4ccf2a
cuonghv <cuonghv201095@gmail.com>
2023-01-27 17:49:12 +0900

    IO file

---
+++ basic/IO_File/IO_File/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+
+namespace IO_File
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public const string CALIBRATION_FILE = "calibration.json"; // file calibration đọc từ file
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(File.Exists(CALIBRATION_FILE))
+            {
+                string content = File.ReadAllText(CALIBRATION_FILE);
+                this.textBox1.Text = content;
+            }
+            else
+            {
+                MessageBox.Show("File not found", "Information");
+                File.Create(CALIBRATION_FILE);
+            }
+        }
+    }
+}
