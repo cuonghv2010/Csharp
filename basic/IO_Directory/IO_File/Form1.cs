f3100454771b235f90d8c40fd4836cb6287f35e7
cuonghv2010 <cuonghv201095@gmail.com>
2023-01-31 17:01:26 +0900

    IO_Directory
    Signed-off-by: cuonghv2010 <cuonghv201095@gmail.com>

---
+++ basic/IO_Directory/IO_File/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System.IO;
+
+namespace IO_File
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public const string CALIBRATION_FILE = "calibration.json"; // file calibration đọc từ file
+        string settingsFolder = "";
+        string settingFilePath = "";
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Directory.Exists(settingsFolder))
+            {
+                if (File.Exists(settingFilePath))
+                {
+                    string content = File.ReadAllText(settingFilePath);
+                    if (content != "")
+                    {
+                        this.textBox1.Text = content;
+                    }
+                    else
+                    {
+                        MessageBox.Show("File empty", "Information");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("File not found", "Information");
+                    File.Create(settingFilePath);
+                }
+            }
+            else
+            {
+                Directory.CreateDirectory(settingsFolder);
+
+                MessageBox.Show("Setting folder not found, creat setting folder ", "Information");
+            }
+            textBox2.Text = settingsFolder;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (File.Exists(settingFilePath))
+            {
+                string content = this.textBox1.Text;
+                if (content != "")
+                {
+                    File.WriteAllText(settingFilePath, content);
+                    MessageBox.Show("Saved to " + settingFilePath, "Information");
+                }
+                else
+                {
+                    MessageBox.Show("Text empty", "Information");
+                }
+            }
+            else
+            {
+                MessageBox.Show("File not found", "Information");
+                File.Create(CALIBRATION_FILE);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+         settingsFolder = Directory.GetCurrentDirectory() + "\\settings";
+         settingFilePath = settingsFolder + "\\" + CALIBRATION_FILE;
+            if (Directory.Exists(settingsFolder))
+            {
+                if (File.Exists(settingFilePath))
+                {
+                    string content = File.ReadAllText(settingFilePath);
+                    if (content != "")
+                    {
+                        this.textBox1.Text = content;
+                    }
+                    else
+                    {
+                        MessageBox.Show("File empty", "Information");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("File not found", "Information");
+                    File.Create(settingFilePath);
+                }
+            }
+            else
+            {
+                Directory.CreateDirectory(settingsFolder);
+
+                MessageBox.Show("Setting folder not found, creat setting folder ", "Information");
+            }
+            textBox2.Text = settingsFolder;
+        }
+    }
+}
